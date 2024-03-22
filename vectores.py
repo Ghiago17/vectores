8e0d017d7cec29ab9ab78f3eaca0e8dca4721c8a
Jay-z <160241067+Ghiago17@users.noreply.github.com>
2024-03-22 08:48:44 -0500

    Add files via upload

---
+++ vectores.py
@@ -0,0 +1,34 @@
+i=[]
+Frutas = ['manzana','pera','banano','melon','naranja','mandarina']
+length = len(i)
+print(length)
+length = len(Frutas)
+print(length)
+
+IN= True,False
+print(Frutas[0])
+print(Frutas[2])
+print(Frutas[5])
+
+Datos_Personales = ['nombre', 'edad', 'altura', 'estado civil', 'direccion']
+Datos_Personales.append('ID')
+print(Datos_Personales)
+
+it_Companies = ['facebooki','google','microsoft','apple','ibm','oracle','amazon']
+it_Companies.insert(0,'dell')
+print(it_Companies)
+
+it_Companies.sort()
+print(it_Companies)
+
+it_Companies.reverse()
+print(it_Companies)
+
+it_Companies.pop(0)
+print(it_Companies)
+
+del it_Companies[0]
+print(it_Companies)
+
+it_Companies.clear()
+print(it_Companies)
